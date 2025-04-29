136a0d73bf44947e12c3c5caa6aa91ef6a69bdc2
shaay98 <nrichardson567@icloud.com>
2025-04-29 02:21:03 +0000

    react webpack first commit

---
+++ my-app/src/App.jsx
@@ -0,0 +1,11 @@
+import Greeting from "./components/greeting.jsx";
+
+export default function App() {
+  return (
+     <div />
+  );
+}
+
+export default function App() {
+  return <Greeting />;
+}
---
+++ my-app/src/components/greeting.jsx
@@ -0,0 +1,5 @@
+import React, {useState} from "react"
+
+export default function Greeting() {
+  return <h1>Hello, React world!</h1>;
+}
